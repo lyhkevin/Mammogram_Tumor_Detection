8283613a1023ccc865113473e68c018a97915fb9
Li Yonghao <liyh2022@shanghaitech.edu.cn>
2024-01-29 09:43:40 +0800

    Update postprocessing.py

--- utils/postprocessing.py
+++ utils/postprocessing.py
@@ -35,8 +35,8 @@ def crop_centered_box(img, x_center, y_center, width, height):
     return cropped
 
 if __name__ == '__main__':
-    subject_paths = glob('dataset/cropped_NAC/*')
-    save_root = 'dataset/NAC_tumor/'
+    subject_paths = glob('dataset/cropped_Surgical/*')
+    save_root = 'dataset/Surgical_tumor/'
     for subject_path in tqdm(subject_paths):
         id = subject_path.split('/')[-1]
         print(id)
@@ -51,7 +51,6 @@ if __name__ == '__main__':
                 with open(view + 'tumor.txt', 'r') as file:
                     line = file.readline().strip()
                     _, x_center, y_center, width, height = map(float, line.split())
-                    
                 if view_name[0] == 'L':
                     Left_tumor_num = Left_tumor_num + 1
                     Left_tumor_size += width * height
@@ -73,13 +72,22 @@ if __name__ == '__main__':
                 shutil.rmtree(save_root + id)
                 print('remove ' + save_root + id)
         else:
-            if Left_tumor_size >= Right_tumor_size:
-                right_imgs = glob(save_root + id + '/*/R*.png')
-                print(right_imgs)
-                for right_img in right_imgs:
-                    os.remove(right_img)
+            if Left_tumor_num != Right_tumor_num:
+                if Left_tumor_num > Right_tumor_num:
+                    right_imgs = glob(save_root + id + '/*/R*.png')
+                    for right_img in right_imgs:
+                        os.remove(right_img)
+                else:
+                    left_imgs = glob(save_root + id + '/*/L*.png')
+                    for left_img in left_imgs:
+                        os.remove(left_img)
             else:
-                left_imgs = glob(save_root + id + '/*/L*.png')
-                print(left_imgs)
-                for left_img in left_imgs:
-                    os.remove(left_img)
+                if Left_tumor_size >= Right_tumor_size:
+                    right_imgs = glob(save_root + id + '/*/R*.png')
+                    for right_img in right_imgs:
+                        os.remove(right_img)
+                else:
+                    left_imgs = glob(save_root + id + '/*/L*.png')
+                    for left_img in left_imgs:
+                        os.remove(left_img)
+
